4f2c8c6443bacd0b008d9f91796b4134b743ced6
Andreas Iwerbo <andreas@iwerbo.se>
2019-01-22 12:18:44 +0100

    worked on popup

--- data/script.js
+++ data/script.js
@@ -1,1 +1,51 @@
 /* JS för vårt grupparbete */
+
+let descriptionText;
+
+
+//EventListener  "spara"-button and insert Ptag when saved. The Ptag should also be inserted to main page. make description editible.
+
+let saveButton = document.querySelector('.popup__saveButton');
+saveButton.addEventListener('click', function(e){
+    
+    descriptionText = document.querySelector('.popup__textarea');
+    let description = descriptionText.value;
+    console.log(descriptionText)
+
+    let hideTextArea = document.querySelector('.popup__textarea');
+    hideTextArea.classList.add('displayNone');
+
+    let popup__saveCloseContainer = document.querySelector('.popup__saveCloseContainer');
+
+    popup__saveCloseContainer.classList.add('displayNone');
+
+    let insertText = document.querySelector('.popup__descriptionFieldContainer');
+    let pTag = document.createElement('p');
+    pTag.classList.add('popup__Ptag')
+    pTag.textContent = description;
+    insertText.appendChild(pTag);
+
+});
+
+
+
+// change to correct textArea
+
+function removeFakeTextAndInsertTextArea(){
+
+let fakteTextDiv = document.querySelector('.popup__fakeTextDiv');
+
+fakteTextDiv.addEventListener('click', function(e){
+    fakteTextDiv.classList.add('displayNone');
+
+    let popup__textarea = document.querySelector('.popup__textarea');
+    let popup__saveCloseContainer = document.querySelector('.popup__saveCloseContainer');
+
+    popup__saveCloseContainer.classList.remove('displayNone');
+    popup__textarea.classList.remove('displayNone');
+
+})
+}
+
+
+removeFakeTextAndInsertTextArea();
