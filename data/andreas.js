08a8c0f49dc351bc887f34ea7945bf0be3643f59
Andreas Iwerbo <andreas@iwerbo.se>
2019-01-29 11:07:00 +0100

    huge changes in andreas.js

--- data/andreas.js
+++ data/andreas.js
@@ -1,6 +1,14 @@
 
 let textArray = [];
 let titleArray = [];
+let textField = document.querySelector('.popup__textField')
+let textArea = document.querySelector('.popup__textarea');
+let popupWindow = document.querySelector('.popup-container');
+let popupPtag = document.querySelector('.popup__pTag');
+let abortButtons = document.querySelectorAll('.popup__close');
+let saveButton = document.querySelector('.popup__saveButton');
+let textFromTextField = textField.value;
+let textFromTextArea = textArea.value;
 
     let todoObj = {
         todos: [],
@@ -37,240 +45,110 @@ let titleArray = [];
 
 
 
-function saveText(){
-    let selectText = document.querySelector('.popup__textarea');
-    if(selectText.value === ''){
-        //emptyTextField();
-        
-    }
-    
+function saveTextEvent(){
+   
+    saveButton.addEventListener('click', saveText);
 
-    let selectSaveButton = document.querySelector('.popup__saveButton');
-    selectSaveButton.addEventListener('click', function(e){
-        let selectText = document.querySelector('.popup__textarea');
-        // Textarean - Description.
-        let selectSaveButton = document.querySelector('.popup__saveButton');
-        let selectAbortButton = document.querySelector('.popup__close');
-        let selectInputText = document.querySelector('.popup__textField');
-        // Titletexten.
-
-        
-
-        let text = selectText.value;
-        let inputText = selectInputText.value;
-        console.log(inputText)
-        textArray.push(text);
-        titleArray.push(inputText);
-        console.log(titleArray);
-        console.log(textArray);
-
-        text = text.replace(/\r?\n/g, '<br/>');
-
-        let pTag = document.createElement('p');
-        pTag.innerHTML = text;
-        let selectContainer = document.querySelector('.popup__descriptionFieldContainer');
-        selectContainer.appendChild(pTag);
-        selectText.style.display = 'none';
-        selectSaveButton.style.display = 'none';
-        selectAbortButton.style.display = 'none';
-        pTag.classList.add('popup__pTag');
-        selectText = '';
-
-        let cardSelector = document.querySelectorAll('.popup__saveButton');
-        console.log(cardSelector)
-        console.log(e.target)
-
-        let createDiv = document.createElement('div');
-        let createPTitle = document.createElement('p');
-        let createPdesc = document.createElement('p');
-
-        for(let i = 0; i < cardSelector.length; i++){
-            if(cardSelector[i].classList[1] === 'popup__saveButton--todo'){
-                let identifier = 'todo';
-                createDiv.classList.add('boards__cards__todo');
-                let selectBoardItemContainer = document.querySelector('.boards__iteamsContainerTodo');
-                selectBoardItemContainer.appendChild(createDiv);
-                cardSelector[i].classList.remove('popup__saveButton--todo');
-                let selectDiv = document.querySelector('.boards__cards__todo')
-                let title = createPTitle.textContent = titleArray[titleArray.length -1];
-                let desc = createPdesc.textContent = textArray[textArray.length -1];
-                /* selectDiv.appendChild(createPTitle);
-                selectDiv.appendChild(createPdesc); */
-                
-                todoObj.addTodos(title, desc);
-                
-                console.log(todoObj.todos);
-                
-
-    
-            }
-            else if(cardSelector[i].classList[1] === 'popup__saveButton--working'){
-                let identifier = 'working';
-                createDiv.classList.add('boards__cards__working');
-                let selectBoardItemContainer = document.querySelector('.boards__iteamsContainerWorking');
-                selectBoardItemContainer.appendChild(createDiv);
-                cardSelector[i].classList.remove('popup__saveButton--working');
-                let title = createPTitle.textContent = titleArray[titleArray.length -1];
-                let desc = createPdesc.textContent = textArray[textArray.length -1];
-                // WorkingArray med objekt 
-                workingObj.addWorkings(title, desc);
-                console.log(workingObj.workings);
-
-            }
-            else if(cardSelector[i].classList[1] === 'popup__saveButton--done'){
-                let identifier = 'done';
-                createDiv.classList.add('boards__cards__done');
-                let selectBoardItemContainer = document.querySelector('.boards__iteamsContainerDone');
-                selectBoardItemContainer.appendChild(createDiv);
-                cardSelector[i].classList.remove('popup__saveButton--done');
-                let title = createPTitle.textContent = titleArray[titleArray.length -1];
-                let desc = createPdesc.textContent = textArray[textArray.length -1];
-                // DoneArray med objekt 
-
-                doneObj.addDones(title, desc);
-                console.log(doneObj.dones);
-               
-            }
-        }
-
-        editText();
-        
-    })
-    
 }
 
-
-
-/* function createObjs(identifier, title, desc){
-
-
-    let todoArr = [];
-    let workingArr = [];
-    let doneArr = [];
-    console.log(identifier)
-    console.log(title);
-    console.log(desc)
-
-    if(identifier === 'todo'){
-        let obj = {};
-        todoArr.push(title)
-        console.log(todoArr)
-    }
-    else if(identifier === 'working'){
-
-    }
-    else if(identifier === 'done'){
-
-    }
-
-} */
-
 function editText(){
-    
-    let selectPTag = document.querySelectorAll('.popup__pTag');
-        
-    for(let i = 0; i < selectPTag.length; i++){
-        selectPTag[i].addEventListener('click', function(){
-            
-            let selectTextArea = document.querySelector('.popup__textarea');
-            let selectSaveButton = document.querySelector('.popup__saveButton');
-            let selectAbortButton = document.querySelector('.popup__close');
-            let selectInputText = document.querySelector('.popup__textField');
-
-            selectTextArea.style.display = 'block';
-            selectSaveButton.style.display = 'block';
-            selectAbortButton.style.display = 'block';
-
-            let selectContainer = document.querySelector('.popup__descriptionFieldContainer').lastChild;
-            selectTextArea.value = textArray[textArray.length -1]; 
-            selectContainer.remove(selectContainer);
-
-            // lägg till så att titelfältet rensas.
-            
-        });
-    }
-
     
 
 }
 
 function abortText(){
-
-    let selectAbortButton = document.querySelector('.popup__close');
-    let selectAbortButtonRight = document.querySelector('.popup__closeUpRight');
-
-    selectAbortButton.addEventListener('click', function(){
-        let selectPopup = document.querySelector('.popup-container');
-        let selectTextArea = document.querySelector('.popup__textarea');
-        let selectSaveButton = document.querySelector('.popup__saveButton');
-        selectPopup.classList.add('displayNone');
-        selectPopup.classList.remove('displayBlock');
-        // clear popup window
-        let selectContainer = document.querySelector('.popup__descriptionFieldContainer').lastChild;
-        selectContainer.remove(selectContainer);
-        selectTextArea.style.display = 'block';
-        selectSaveButton.style.display = 'block';
-        selectAbortButton.style.display = 'block';
-        selectTextArea.value = '';
-        
-    })
-    selectAbortButtonRight.addEventListener('click', function(){
-        let selectPopup = document.querySelector('.popup-container');
-        let selectTextArea = document.querySelector('.popup__textarea');
-        let selectSaveButton = document.querySelector('.popup__saveButton');
-        selectPopup.classList.add('displayNone')
-        selectPopup.classList.remove('displayBlock');
-        // clear popup window
-        let selectContainer = document.querySelector('.popup__descriptionFieldContainer').lastChild;
-        selectContainer.remove(selectContainer);
-        selectTextArea.style.display = 'block';
-        selectSaveButton.style.display = 'block';
-        selectAbortButton.style.display = 'block';
-        selectTextArea.value = '';
-    })
     
-    
-    
-}
-
-function addCard(){
-
-    
-    
+    for(let i = 0; i < abortButtons.length; i++){
+        abortButtons[i].addEventListener('click', abortPopup)
+    }
+    showArea();
    
+}
+
+function showArea(){
+
+    popupPtag = document.querySelector('.popup__pTag');
+    popupPtag.addEventListener
+    ('click', showTextArea);
 
 }
 
-/* function emptyTextField(){
-    let selectText = document.querySelector('.popup__textarea');
+function removeClasLists(){
+        saveButton = document.querySelector('.popup__saveButton')
+        saveButton.classList.remove('popup__saveButton--todo');
+        saveButton.classList.remove('popup__saveButton--working');
+        saveButton.classList.remove('popup__saveButton--done');
+}
+
+function getTextFromFieldAndArea(){
+
     
-        
-        selectText.classList.add('displayNone');
-        let selectContainer = document.querySelector('.popup__descriptionFieldContainer');
-        let createP = document.createElement('p');
-        createP.textContent = 'Insert text here';
-        selectContainer.appendChild(createP);
-
-        createP.addEventListener('click', function(e){
-            createP.textContent = '';
-            selectText.classList.add('displayBlock');
-            
-        })
-
-        
     
-} */
 
-saveText();
-abortText()
+}
+
+function saveText(e){
+    let eventTarget = e.target;
+    textFromTextField = textField.value;
+    textFromTextArea = textArea.value;
+    textFromTextArea = textFromTextArea.replace(/\r?\n/g, '<br/>');
+    
+    
+    if(eventTarget.classList[1] === 'popup__saveButton--todo'){
+        todoObj.addTodos(textFromTextField, textFromTextArea);
+        console.log(todoObj.todos)
+        // spara i objekt
+        
+    }
+    if(eventTarget.classList[1] === 'popup__saveButton--working'){
+        workingObj.addWorkings(textFromTextField, textFromTextArea);
+        console.log(workingObj.workings)
+        // spara i objekt
+        
+    }
+    if(eventTarget.classList[1] === 'popup__saveButton--done'){
+        doneObj.addDones(textFromTextField, textFromTextArea);
+        console.log(doneObj.dones)
+        // spara i objekt
+        
+    }
+}
+
+
+function showTextArea(e){
+    popupPtag = document.querySelector('.popup__pTag');
+    popupPtag.classList.add('displayNone');
+    popupPtag.classList.remove('displayBlock');
+    textArea = document.querySelector('.popup__textarea');
+    textArea.classList.add('displayBlock');
+    textArea.classList.remove('displayNone');
+    
+}
+
+
+function abortPopup(e){
+    popupWindow = document.querySelector('.popup-container');
+    popupWindow.classList.add('displayNone');
+    popupWindow.classList.remove('displayBlock');
+    textArea.value = '';
+    textArea.classList.add('displayNone');
+    textArea.classList.remove('displayBlock');
+    popupPtag.classList.add('displayBlock');
+    popupPtag.classList.remove('displayNone');
+    
+}
+
 
 let exportObject = {
 
-    saveText: saveText,
+    saveTextEvent: saveTextEvent,
     editText: editText,
     abortText: abortText,
-    
-
+    removeClasLists: removeClasLists,
+    getTextFromFieldAndArea: getTextFromFieldAndArea,
+    todoObj: todoObj,
+    workingObj: workingObj,
+    doneObj: doneObj,
 };
 
 export default exportObject;
--- data/fredrik.js
+++ data/fredrik.js
@@ -5,28 +5,34 @@ import andreas from './andreas.js';
 let getPopup = document.querySelector('.popup-container');
 
 // Add cards
+
 addIteam();
 function addIteam(){
+  andreas.abortText();
+  andreas.saveTextEvent();
   let getListenerBtn = document.querySelectorAll('.boards__addCardBtn');
   for (let i = 0; i < getListenerBtn.length; i++) {
     let getTargetBtn = getListenerBtn[i];
     getTargetBtn.addEventListener('click', function(e){
-
+      
       let saveButton = document.querySelector('.popup__saveButton')
       console.log(saveButton)
       if(e.target.classList[1] === 'boards__addCardBtnTodo'){
+        andreas.removeClasLists();
         saveButton.classList.add('popup__saveButton--todo')
       }
       else if(e.target.classList[1] === 'boards__addCardBtnWorking'){
+        andreas.removeClasLists();
         saveButton.classList.add('popup__saveButton--working')
       }
       else if(e.target.classList[1] === 'boards__addCardBtnDone'){
+        andreas.removeClasLists();
         saveButton.classList.add('popup__saveButton--done')
       }
 
       console.log(e.target)
       getPopup.classList.add('displayBlock');
-
+      
     // Datum och tid --------------------------------------------------------------------------------------------
     let getDatePlace = document.querySelector('.popup__todaysDate');
 
@@ -78,6 +84,8 @@ function addIteam(){
     // Titel i popupen töms
     let getTitelPopup = document.querySelector('.popup__textField');
     getTitelPopup.value = '';
+    
     });
+    
   }
 }
