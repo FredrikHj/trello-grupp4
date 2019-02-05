b82545b4c6026c6a2253be0bb2d3cc6ceb8d89d5
unknown <bjornetorp@hotmail.com>
2019-02-05 11:22:14 +0100

    added edit button

--- bundle.js
+++ bundle.js
@@ -14,7 +14,7 @@
       let titleText = todo.titles;
       let descriptionText = todo.descs;
       let identifier = todo.identifier;
-      
+
 
       //Creating a new div (the card)
       let card = document.createElement('div');
@@ -33,16 +33,27 @@
       title.classList.add('card__headerDiv__title');
       header.appendChild(title);
 
+      //Adding edit button
+      let editButton = document.createElement('button');
+      editButton.classList.add('card__headerDiv__editButton');
+      editButton.setAttribute('id', identifier);
+      header.appendChild(editButton);
+      let editIcon = document.createElement('i');
+      editIcon.classList.add('material-icons');
+      editIcon.classList.add('card__headerDiv__editButton__icon');
+      editIcon.textContent = 'edit';
+      editButton.appendChild(editIcon);
+
       //Adding delete button
       let deleteButton = document.createElement('button');
       deleteButton.classList.add('card__headerDiv__deleteButton');
-        deleteButton.setAttribute('id', identifier);
+      deleteButton.setAttribute('id', identifier);
       header.appendChild(deleteButton);
-      let icon = document.createElement('i');
-      icon.classList.add('material-icons');
-      icon.classList.add('material-icons__delete');
-      icon.textContent = 'clear';
-      deleteButton.appendChild(icon);
+      let deleteIcon = document.createElement('i');
+      deleteIcon.classList.add('material-icons');
+      deleteIcon.classList.add('card__headerDiv__deleteButton__icon');
+      deleteIcon.textContent = 'clear';
+      deleteButton.appendChild(deleteIcon);
 
       //Adding text icon if there is a description
       if (descriptionText) {
@@ -52,28 +63,28 @@
         card.appendChild(textIcon);
       }
 
-      //Adding a container with the arrows
-      let bottomDiv = document.createElement('div');
-      bottomDiv.classList.add('card__bottomDiv');
-      card.appendChild(bottomDiv);
-
-      // -- Left arrow --
-      let leftArrow = document.createElement('i');
-      let getBtnToLeftArrow = document.createElement('button');
-      getBtnToLeftArrow.classList.add('card__bottomDiv__leftArrow');
-      leftArrow.classList.add('material-icons');
-      leftArrow.textContent = 'arrow_back';
-      getBtnToLeftArrow.appendChild(leftArrow);
-      bottomDiv.appendChild(getBtnToLeftArrow);
-
-      // -- Right arrow --
-      let rightArrow = document.createElement('i');
-      let getBtnToRightArrow = document.createElement('button');
-      getBtnToRightArrow.classList.add('card__bottomDiv__rightArrow');
-      rightArrow.classList.add('material-icons');
-      rightArrow.textContent = 'arrow_forward';
-      getBtnToRightArrow.appendChild(rightArrow);
-      bottomDiv.appendChild(getBtnToRightArrow);
+      // //Adding a container with the arrows
+      // let bottomDiv = document.createElement('div');
+      // bottomDiv.classList.add('card__bottomDiv');
+      // card.appendChild(bottomDiv);
+      //
+      // // -- Left arrow --
+      // let leftArrow = document.createElement('i');
+      // let getBtnToLeftArrow = document.createElement('button');
+      // getBtnToLeftArrow.classList.add('card__bottomDiv__leftArrow');
+      // leftArrow.classList.add('material-icons');
+      // leftArrow.textContent = 'arrow_back';
+      // getBtnToLeftArrow.appendChild(leftArrow);
+      // bottomDiv.appendChild(getBtnToLeftArrow);
+      //
+      // // -- Right arrow --
+      // let rightArrow = document.createElement('i');
+      // let getBtnToRightArrow = document.createElement('button');
+      // getBtnToRightArrow.classList.add('card__bottomDiv__rightArrow');
+      // rightArrow.classList.add('material-icons');
+      // rightArrow.textContent = 'arrow_forward';
+      // getBtnToRightArrow.appendChild(rightArrow);
+      // bottomDiv.appendChild(getBtnToRightArrow);
   }
   }
 
